3d6e348738f2cb68a63aa3223e483a61456cd0b2
ThisIsNsh <hadanis.singh@gmail.com>
2020-01-07 16:09:35 +0530

    Files Added

---
+++ README.md
@@ -0,0 +1,11 @@
+# MATLAB-Image-Negative
+
+### Image Processing
+
+Finding Negative of a Coloured Image.
+--------
+* I have found maximum intensity of a pixel and subtracted other intensities from it .
+
+<img src="im1.PNG">  
+
+
---
+++ negative.m
@@ -0,0 +1,22 @@
+﻿function im = negative(arr) 
+    max = 0;
+    for row = 1:size(arr,1)
+        for col = 1:size(arr,2) 
+            for pix = 1:size(arr,3) 
+                if (arr(row,col,pix)>max)
+                    max = arr(row,col,pix);
+                end
+            end
+        end
+    end
+    
+    for row = 1:size(arr,1)
+        for col = 1:size(arr,2) 
+            for pix = 1:size(arr,3) 
+                arr(row,col,pix) = max - arr(row,col,pix);
+            end
+        end
+    end
+    
+    im = arr;
+end
